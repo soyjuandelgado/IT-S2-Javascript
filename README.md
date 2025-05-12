0a9b4e33be9331fd1f86ed4356e1aab828d35a86
Juan Delgado <soy.juan.delgado@gmail.com>
2025-05-12 12:48:49 +0200

    Inicial

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # IT-S2-Javascript
+
+## 📄Descripción
+
+
+## 💻Tecnologías Utilitzadas
+
+- 
+
+## 📋Requisitos
+
+
+## 🛠️Instalación
+
+
+## ▶️Ejecución
+
+1. Visitar la direccion de la web.
+
+[Demo en vivo](https://soyjuandelgado.github.io/IT-S1-Maquetacion/)
+
+## 🌐Despliegue
+
+No aplica.
+
+## 🤝Contribuciones
+
+No aplica.
---
+++ src/main.js
@@ -0,0 +1,16 @@
+
+console.log("SUMA");
+// SUMA
+let a = 3;
+let b = 5;
+let add = (a, b) => a + b; //suma es la funcion, no el retorno de la misma
+let res = add(a,b);
+console.log("Resultado", res);
+
+//RANDOM
+console.log("RANDOM");
+let randomNumber = () => Math.floor(Math.random() * 101);
+console.log(randomNumber());
+
+// GREET
+console.log("GREET");
