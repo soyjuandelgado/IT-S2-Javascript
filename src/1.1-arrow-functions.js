f76abb2f8273a806cca3ffe5756e2aae51a9975f
Juan Delgado <soy.juan.delgado@gmail.com>
2025-05-20 12:32:15 +0200

    feat: show 1.1 operation results

--- src/1.1-arrow-functions.js
+++ src/1.1-arrow-functions.js
@@ -1,33 +1,36 @@
 
 function arrowFunctions(){
-    //const salida = document.getElementById("salida")
+    const salida = document.getElementById("salida")
 
     console.log("SUMA");
-    //salida.innerHTML = "SUMA<br>";
+    salida.innerHTML = "SUMA<br>";
     // SUMA
     let a = 3;
     let b = 5;
     let add = (a, b) => a + b; //suma es la funcion, no el retorno de la misma
     let res = add(a,b);
     console.log("Resultado", res);
-    //salida.innerHTML += `Resultado ${res}<br>`;
+    salida.innerHTML += `Resultado ${res}<br>`;
 
     //RANDOM
     console.log("RANDOM");
+    salida.innerHTML += "RANDOM<br>";
     //Math.random: aleatorios entre 0 y 1
     //Math.floor: devuelve numeros enteros
     let randomNumber = () => Math.floor(Math.random() * 101); 
     console.log(randomNumber());
+    salida.innerHTML += randomNumber() + "<br>";
 
     // GREET
     console.log("GREET");
-
+    salida.innerHTML += "GREET<br>";
     class Person{
         constructor(nombre){
             this.nombre = nombre;
         }
 
-        greet = () => console.log(`Hola, ${this.nombre}`)
+        greet = () => {console.log(`Hola, ${this.nombre}`)
+                salida.innerHTML += `Hola, ${this.nombre}` + "<br>";}   
     }
 
     const persona = new Person("Juan");
@@ -36,9 +39,12 @@ function arrowFunctions(){
     //Nivel 2
 
     console.log("printNumbers");
+    salida.innerHTML += "printNumbers<br>";
 
     function printNumbers(nums) {
-        for (let i = 0; i < nums.length; i++) () => console.log(nums[i]);
+        for (let i = 0; i < nums.length; i++) () => {console.log(nums[i]);
+                                                salida.innerHTML += nums[i] + "<br>";
+        };
     }
 
     let numbers = [3, 5, 7, 4, 2];
@@ -47,6 +53,9 @@ function arrowFunctions(){
     //Nivel 3
 
     console.log("setTimeout");
+    salida.innerHTML += "setTimeout<br>";
 
-    setTimeout( () => console.log("Mensaje retardado.") , 3000 );
+    setTimeout( () => {console.log("Mensaje retardado.")
+        salida.innerHTML += "Mensaje retardado.<br>";
+    } , 3000 );
 }
